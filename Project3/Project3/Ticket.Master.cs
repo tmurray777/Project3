6dcd3dc5d2e77e9514284a8da95adc6b75a54bcd
Tj <tue64727@temple.edu>
2016-10-18 19:15:42 -0400

    Begin Project3

--- Project3/Project3/Ticket.Master.cs
+++ Project3/Project3/Ticket.Master.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Homework_7
+namespace Project3
 {
-    public partial class Login : System.Web.UI.Page
+    public partial class Ticket : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
